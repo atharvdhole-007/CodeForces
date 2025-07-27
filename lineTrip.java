17bee8d9676763b4a67a780c95c9b9c597557453
Atharv Dhole <atharv.dhole241@vit.edu>
2025-07-27 14:14:31 +0530

    line Trip problem solved

--- lineTrip.java
+++ lineTrip.java
@@ -1,7 +1,36 @@
 package CodeForces;
 
+import java.util.Scanner;
+
 public class lineTrip {
     public static void main(String[] args) {
-        
+        Scanner scn = new Scanner(System.in);
+
+        int t = scn.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = scn.nextInt();
+            int x = scn.nextInt();
+            int[] arr = new int[n];
+
+            for (int j = 0; j < n; j++) {
+                arr[j] = scn.nextInt();
+            }
+            int max = arr[0];
+
+            for (int j = 0; j < arr.length - 1; j++) {
+                int diff = arr[j+1] - arr[j];
+                if (diff > max) {
+                    max = diff;
+                }
+
+            }
+            int edge = 2*(x - arr[n-1]);
+            if (edge > max) {
+                max = edge;
+            }
+            System.out.println(max);
+        }
+        scn.close();
     }
 }
