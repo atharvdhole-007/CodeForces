119aaa444cba68acd995e11b3824f567eb63e677
Atharv Dhole <atharv.dhole241@vit.edu>
2026-04-19 14:54:15 +0530

    one simple problem of div4 A is solved

---
+++ seats.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreElements()) {
+            st = new StringTokenizer(br.readLine());
+        }
+        return st.nextToken();
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    public static void main(String[] args) throws Exception {
+        int t = 1;
+        t = nextInt(); 
+        while (t-- > 0) {
+            solve();
+        }
+    }
+
+    static void solve() throws Exception {
+        // Write your logic here
+        int n =nextInt();
+        String s = next();
+
+        for (int i = 0; i < n; i++) {
+            if (n -i > 3) {
+                
+            }
+        }
+    }
+}
